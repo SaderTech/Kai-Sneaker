3ccc743ec821a38261770276cee65025ada733b0
Quang Hao <haoling447@gmail.com>
2026-03-10 16:06:11 +0700

    Tạo ra các DTO Brand, Ctegory, HomeBrandSection, HomePage để phục vụ cho trang Home

--- backend/src/main/java/com/quanghao/backend/dto/HomePageDTO.java
+++ backend/src/main/java/com/quanghao/backend/dto/HomePageDTO.java
@@ -1,4 +1,20 @@
 package com.quanghao.backend.dto;
 
+import com.quanghao.backend.entity.Brand;
+import com.quanghao.backend.entity.Category;
+import lombok.Builder;
+import lombok.Data;
+import java.util.List;
+
+@Data
+@Builder
 public class HomePageDTO {
+// Dữ liệu cho Narbar
+    private List<BrandDTO> navbarBrands;
+    private List<CategoryDTO> navbarCategories;
+//Dữ liệu cho các List sản phẩm gợi ý
+    private List<ProductListDTO> featuredProducts;
+    private List<ProductListDTO> newArrivals;
+//Dữ liệu cho các List sản phẩm của các brand
+    private List<HomeBrandSectionDTO> brandSections;
 }
